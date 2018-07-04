5ae209140024f40c3ffda86d28a2718ccdb3b7b9
Hubert <hubert.thieriot@gmail.com>
2018-07-04 12:34:49 +0800

    -adding china row reserves

---
+++ instances/charts/energy/china_vs_row_coal_reserves/content.js
@@ -0,0 +1,57 @@
+
+// Create the chart
+chart = new Highcharts.Chart({
+  chart: {
+    renderTo: 'china_vs_row_coal_reserves_container',
+    type: 'pie'
+  },
+  title: {
+    text: 'Top-5 Coal Reserves'
+  },
+  plotOptions: {
+    pie: {
+      shadow: false,
+      colors: ["#560008","#ac0010","#c54d58","#d68088","#e6b3b7","#BDBFC3"],
+      dataLabels: {
+          enabled: true,
+          format: '<b>{point.name}</b><br>{point.percentage:.1f} %',
+          distance: 10,
+          connectorWidth:0,
+      }
+    }
+  },
+
+
+
+  legend: {
+    enabled: false
+  },
+  credits: {
+    enabled: false
+  },
+  exporting: {
+    enabled: false
+  },
+  tooltip: {
+    formatter: function() {
+      return '<b>'+ this.point.name +'</b>: '+ this.y +' bn tonnes';
+    }
+  },
+  series: [{
+    name: 'Coal reserves',
+
+    data: [
+      ["United States",250916],
+      ["Russia",160364],
+      ["Australia",144818],
+      ["China",138819],
+      ["India",97728],
+      ["Others",1139331]],
+      size: '60%',
+      innerSize: '50%',
+      showInLegend:true,
+      dataLabels: {
+        enabled: true
+      }
+    }]
+  });

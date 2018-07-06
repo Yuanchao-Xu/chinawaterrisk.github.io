c5489308af698306e7018401f30747b6514174f1
Hubert <hubert.thieriot@gmail.com>
2018-07-06 09:48:33 +0800

    -add dry11 sown land

---
+++ instances/charts/agriculture/pie/dry11_sown_land/content.js
@@ -0,0 +1,77 @@
+
+$( document ).ready(function() {
+// Create the chart
+chart = new Highcharts.Chart({
+  chart: {
+    renderTo: 'dry11_sown_land_container',
+    type: 'pie',
+    style: {
+      fontFamily: 'Arial Narrow'
+       },
+    height:'95%'
+  },
+  title: {
+    text: 'Exposure'
+  },
+  plotOptions: {
+    pie: {
+      shadow: false,
+      colors: ["#7C8388","#094677","#0D77B9"],
+      dataLabels: {
+          enabled: true,
+          format: '<b>{point.name}</b><br>{point.percentage:.1f} %',
+          distance: 10,
+          //color: 'white',
+          connectorWidth:0,
+          style: {
+            //color:'white',
+            fontSize: '12px',
+            fontWeight:300,
+            textOutline: false
+          }
+      }
+    }
+  },
+
+  legend: {
+    enabled: false
+  },
+  credits: {
+    enabled: false
+  },
+  exporting: {
+    enabled: false
+  },
+  tooltip: {
+    formatter: function() {
+      return '<b>'+ this.point.name +'</b>: '+ this.y +' bn tonnes';
+    }
+  },
+
+series: [{
+  name: 'Coal reserves2',
+  data: [
+    {name:"<500",y:40083.42, color:"#9F171E"},
+    {name:"500-1000",y:15994.86, color:"#7C8388"},
+    {name:"1000-2000",y:57744.81, color:"#094677"},
+    {name:">2000",y:52826.46, color:"#0D77B9"}],
+  size: '60%',
+  innerSize: '50%',
+  dataLabels: {
+    enabled:false
+    }
+  },{
+  name: 'Coal reserves',
+  data: [
+    ["Dry 11",43043.83],
+    ["At Risk 7",56078.28],
+    ["Safe 13",67527.44]],
+    size: '68%',
+    innerSize: '65%',
+    showInLegend:true,
+    dataLabels: {
+      enabled: true
+    }
+  }],
+  });
+});

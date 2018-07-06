c0c48450e50572997bf16d193809045c02950301
ht <ht@erprofiler.com>
2018-07-06 19:01:25 +0800

    -adding irrigation efficiency

---
+++ instances/charts/agriculture/lines/irrigation_efficiency/content.js
@@ -0,0 +1,138 @@
+
+Highcharts.chart('irrigation_efficiency_container', {
+				credits: {
+					enabled: false
+				},
+				chart: {
+					style: {
+						fontFamily: 'Arial Narrow'
+					},
+					alignTicks: false,
+					spacingTop: 0,
+					spacingRight: 0,
+					spacingBottom: 0,
+					spacingLeft: 0
+				},
+				title: {
+					text: '2000-2016 Irrigation efficiency',
+					align: 'left',
+					style: {
+						color: 'black',
+						fontSize: '20px',
+						fontWeight: 'bold'
+					}
+				},
+				xAxis: {
+					className: 'x-axis',
+					tickInterval: 2,
+					labels: {
+						style: {
+							color: 'black',
+							fontSize:'12px'
+						}
+					},
+					//lineColor: 'black',
+					tickLength: 0
+			//		categories: ['2000', '2005', '2010', '2015', '2020', '2025', '2030']
+				},
+				yAxis: [{
+					className: 'y-axis',
+					labels: {
+						formatter: function() {
+						   return this.value;
+						},
+						style: {
+							color: 'black',
+							fontSize:'12px'
+						}
+					},
+					gridLineColor: '#D3D3D3',
+					// tickInterval: 10000,
+					// min: 0,
+					// max: 70000,
+					 title: {
+					 	text: ''
+					 }
+				}],
+				tooltip: {
+					headerFormat: '<b>{series.name}</b><br/>',
+					pointFormat: '{point.x}: {point.y:.1f}',
+					borderColor: 'black'
+				},
+		//		labels: {
+		//			items: [{
+		//				html: '2014',
+		//				style: {
+		//					left: '610px',
+		//					top: '75px',
+		//					color: 'black',
+		//					fontSize: '15px',
+		//					fontWeight: 'bold'
+		//				}
+		//			}]
+		//		},
+				plotOptions: {
+					series: {
+						label: {
+							connectorAllowed: false
+						},
+						pointStart: 2006
+					},
+					spline: {
+						marker: {
+							enabled: false,
+							symbol: 'circle',
+							radius: 2,
+							states: {
+								hover: {
+									enabled: true
+								}
+							}
+						}
+					},
+					line: {
+						marker: {
+							enabled: false,
+
+							symbol: 'circle',
+							radius: 2,
+							states: {
+								hover: {
+									enabled: true
+								}
+							}
+						}
+					}
+				},
+				legend: {
+					enabled: false
+				},
+				credits: {
+					enabled: false
+				},
+				exporting: {
+					enabled: false
+				},
+				series: [{
+					//type: 'spline',
+					name: 'Irrigation efficiency',
+					data: [[	2000,7185	],
+          [	2001,7185	],
+          [	2002,6975	],
+          [	2003,6450	],
+          [	2004,6750	],
+          [	2005,6720	],
+          [	2006,6735	],
+          [	2007,6510	],
+          [	2008,6525	],
+          [	2009,6465	],
+          [	2010,6315	],
+          [	2011,6225	],
+          [	2012,6060	],
+          [	2013,6270	],
+          [	2014,6030	],
+          [	2015,5910	],
+          [	2016,5700	]],
+					color: '#094677'
+				}]
+			});

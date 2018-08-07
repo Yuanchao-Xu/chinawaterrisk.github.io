3071f85626d7c0d4d90b390b9d1b022ce68a665d
Hubert <hubert.thieriot@gmail.com>
2018-08-07 16:58:21 +0800

    -added scale

--- instances/maps/water_sources/content_mapbox.js
+++ instances/maps/water_sources/content_mapbox.js
@@ -35,3 +35,16 @@ jQuery("input[name='basemap']").each(function(){
 jQuery( document ).ready(function() {
   ns_watersources_map.init();
 });
+
+// Legend
+const legend = document.getElementById('legend_prec_scale');
+const grad = chroma.scale(["#4d0d04","#931019","#d21e32","#e76f62","#f5af90","#f6ccb9","#f7e9e3","#e8f0f4","#c9e0ec","#a9d1e4","#87bfdb","#5ca5cd","#308bbe","#0571b0"])
+    .domain([1,2,3,4,5,6,7,8,9,10,11,12,13,14])
+    .mode('lab');
+
+
+
+
+
+//legend.style.backgroundImage = `linear-gradient(to right, ${grad(0)}, ${grad(14)})`;
+legend.style.backgroundImage = `linear-gradient(to right, #4d0d04, #931019,#d21e32,#e76f62,#f5af90,#f6ccb9,#f7e9e3,#e8f0f4,#c9e0ec,#a9d1e4,#87bfdb,#5ca5cd,#308bbe,#0571b0)`;

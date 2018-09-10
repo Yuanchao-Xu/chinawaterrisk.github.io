26f24997010962e7dac9006c373f56eeed19a58d
Hubert <hubert.thieriot@gmail.com>
2018-09-10 18:44:04 +0200

    -adding gwsw treemap

---
+++ instances/charts/water/treemap/content.js
@@ -0,0 +1,103 @@
+jQuery( document ).ready(function() {
+
+// Create the chart
+chart = new Highcharts.Chart({
+  chart: {
+    renderTo: 'grounwater_surfacewater',
+    style: {
+           fontFamily: 'Arial Narrow'
+       },
+    backgroundColor:"transparent",
+    spacingTop: 30
+  },
+    series: [{
+      type: 'treemap',
+      layoutAlgorithm: 'squarified',
+      data: [{
+              name: 'Groundwater',
+              value: 105.7,
+              color: "#200D0F",
+              percentage: "18%"
+          }, {
+              name: 'Surfacewater',
+              value: 491.2,
+              color: "#149E9A",
+              percentage: "82%"
+          }]
+        }],
+
+    // series: [{
+    //     name: 'Target',
+    //     type: 'polygon',
+    //     name:"Groundwater",
+    //     data: [[153, 42], [149, 46], [149, 55], [152, 60], [159, 70], [170, 77], [180, 70],
+    //         [180, 60], [173, 52], [166, 45]]
+    //       }],
+
+  title: {
+    text:'',
+    //text: 'Top-5 Sown Land',
+    margin: 0
+  },
+  plotOptions: {
+
+    polygon: {
+      shadow: false,
+      dataLabels: {
+          enabled: true,
+          useHTML: true,
+          format: '<center><b>{point.name}</b></br>{point.value} bn m<sup>3</sup></br>{point.percentage}</center>',
+          //format: '{point.name}',
+          //distance: 5,
+          //color: 'white',
+          //connectorWidth:0,
+          align: 'center',
+          //verticalAlign: 'top',
+          style: {
+            //color:'white',
+            fontSize: '12px',
+            //fontWeight:300,
+            textOutline: false,
+          }
+      }
+    },
+
+    treemap: {
+      shadow: false,
+      dataLabels: {
+          enabled: true,
+          useHTML: true,
+          format: '<center><b>{point.name}</b></br>{point.value} bn m<sup>3</sup></br>{point.percentage}</center>',
+          //format: '{point.name}',
+          //distance: 5,
+          //color: 'white',
+          //connectorWidth:0,
+          align: 'center',
+          //verticalAlign: 'top',
+          style: {
+            //color:'white',
+            fontSize: '12px',
+            //fontWeight:300,
+            textOutline: false,
+          }
+      }
+    }
+  },
+
+  legend: {
+    enabled: false
+  },
+  credits: {
+    enabled: false
+  },
+  exporting: {
+    enabled: false
+  },
+  tooltip: {
+    enabled: false
+    // formatter: function() {
+    //   return '<b>'+ this.point.name +'</b>: '+ this.y +' bn tonnes';
+    // }
+  },
+  });
+  });

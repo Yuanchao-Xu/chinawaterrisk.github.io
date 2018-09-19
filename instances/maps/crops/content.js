21dd6acfa888cf7f4e305c76d8349c7e3d2a4ecf
Hubert <hubert.thieriot@gmail.com>
2018-09-19 22:02:22 +0200

    - province-name-g

--- instances/maps/crops/content.js
+++ instances/maps/crops/content.js
@@ -430,6 +430,7 @@ var ns_crops = {
                   .enter()
                   .append('text')
                   .attr('id', 'bubble_text')
+                  .attr('class','province-name-g')
                   .attr('transform', function(d) { return 'translate(' + path.centroid(d) + ')'; })
                   .style('fill','#FFFFFF')
                   .text(function(d) { return d.properties.NAME_1; })
